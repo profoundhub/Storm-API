b794cb4fe745e78a3cada672094d4df14a583b4c
Daniel Lim <hbt@markhamboards.com>
2016-09-09 17:25:27 -0400

    1 x
    1 x

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
 import ReactStormpath, { Router, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IndexRoute, HomeRoute, Route, browserHistory } from 'react-router';
-import { MasterPage, IndexPage, LoginPage, useRouterHistory, RegistrationPage, ProfilePage } from './pages';
+import { IndexRoute, HomeRoute, Route, browserHistory, useRouterHistory  } from 'react-router';
+import { MasterPage, IndexPage, LoginPage, RegistrationPage, ProfilePage } from './pages';
 import createHashHistory from 'history/lib/createHashHistory';
-
-
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 ReactStormpath.init({
   // See the API docs for available configuration options.
 });
 
 ReactDOM.render(
-  <div>
-    <Router history={appHistory}>
-      <HomeRoute path='/' component={MasterPage}>
-        <IndexRoute component={IndexPage} />
-        <LoginRoute path='/login' component={LoginPage} />
-        <Route path='/register' component={RegistrationPage} />
-          <AuthenticatedRoute>
-            <HomeRoute path='/profile' component={ProfilePage}></HomeRoute>
-          </AuthenticatedRoute>
-      </HomeRoute>
-    </Router>
-  </div>,
+  <Router history={createHashHistory({ queryKey: false })}>
+
+    <HomeRoute path='/' component={MasterPage}>
+      <IndexRoute component={IndexPage} />
+      <LoginRoute path='/login' component={LoginPage} />
+      <Route path='/register' component={RegistrationPage} />
+        <AuthenticatedRoute>
+          <HomeRoute path='/profile' component={ProfilePage} />
+        </AuthenticatedRoute>
+    </HomeRoute>
+
+  </Router>,
   document.getElementById('app-container')
 );
