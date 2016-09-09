bff7623e227d6fe5cc2501a2ba94539994b76444
Daniel Lim <hbt@markhamboards.com>
2016-09-09 15:06:18 -0400

    add inputs
    add inputs

--- src/example.js
+++ src/example.js
@@ -4,11 +4,15 @@ class HelloWorld extends React.Component {
   }
 }
 
+// <label for="my-input" class="my-label">My Input</label>
+
 ReactDOM.render(
   <HelloWorld />,
   document.getElementById('hello-world-element')
 
   <Counter from="50" />
   <Counter from={ myVariable } />
+
+  <label htmlFor="my-input" className="my-label">My Input</label>
 
 );
