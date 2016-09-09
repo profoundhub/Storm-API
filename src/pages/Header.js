e351660222842471a525f9709f63f1fe8bf4a39a
Daniel Lim <hbt@markhamboards.com>
2016-09-09 16:05:35 -0400

    ProfilePage.js
    ProfilePage.js

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -17,6 +17,11 @@ export default class Header extends React.Component {
                   <LoginLink />
                 </li>
               </NotAuthenticated>
+              <NotAuthenticated>
+                <li>
+                  <Link to="/register">Create Account</Link>
+                </li>
+              </NotAuthenticated>
             </ul>
           </div>
         </div>
