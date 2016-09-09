af75f59b856b94dfe6e156bc71e9976805ff48d4
Daniel Lim <hbt@markhamboards.com>
2016-09-09 17:41:58 -0400

    back to normal
    back to normal

--- src/app.js
+++ src/app.js
@@ -3,16 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IndexRoute, HomeRoute, Route, browserHistory, useRouterHistory  } from 'react-router';
 import { MasterPage, IndexPage, LoginPage, RegistrationPage, ProfilePage } from './pages';
-import createHashHistory from 'history/lib/createHashHistory';
-
+import createHashHistory from 'history/lib/createHashHistory.js';
+var createBrowserHistory = require('history/lib/createBrowserHistory');
 const history = createBrowserHistory();
-
+// Opt-out of persistent state, not recommended.
+let history = createHashHistory({
+  queryKey: false
+});
 ReactStormpath.init({
   // See the API docs for available configuration options.
 });
 
 ReactDOM.render(
-  <Router history={createBrowserHistory()}>
+  <Router history={history}>
     <HomeRoute path='/' component={MasterPage}>
       <IndexRoute component={IndexPage} />
       <LoginRoute path='/login' component={LoginPage} />
