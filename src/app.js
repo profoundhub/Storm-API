65e9993c848ff497090e2abdb568f1c7a58ae8be
Daniel Lim <hbt@markhamboards.com>
2016-09-09 17:36:29 -0400

    add History
    Add: npm install history react-router@latest

--- src/app.js
+++ src/app.js
@@ -5,22 +5,22 @@ import { IndexRoute, HomeRoute, Route, browserHistory, useRouterHistory  } from 
 import { MasterPage, IndexPage, LoginPage, RegistrationPage, ProfilePage } from './pages';
 import createHashHistory from 'history/lib/createHashHistory';
 
+const history = createBrowserHistory();
+
 ReactStormpath.init({
   // See the API docs for available configuration options.
 });
 
 ReactDOM.render(
-  <Router history={createHashHistory({ queryKey: false })}>
-
+  <Router history={createBrowserHistory()}>
     <HomeRoute path='/' component={MasterPage}>
       <IndexRoute component={IndexPage} />
       <LoginRoute path='/login' component={LoginPage} />
       <Route path='/register' component={RegistrationPage} />
         <AuthenticatedRoute>
-          <HomeRoute path='/profile' component={ProfilePage} />
+          <HomeRoute path='/profile' component={ProfilePage}></HomeRoute>
         </AuthenticatedRoute>
     </HomeRoute>
-
   </Router>,
   document.getElementById('app-container')
 );
